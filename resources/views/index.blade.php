1b24b16655695195df327b2d933b096474ed9a9e
Edwinj10 <Edwinjosealtamirano@gmail.com>
2017-09-20 10:09:23 -0600

    Nuevo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,7 +82,7 @@
                       <span class="icon-bar"></span>
                       <span class="icon-bar"></span>
                     </button>
-                    <img class="img-responsive" id="logo" src="img/CIIEMP-2 negativo-1.png" alt="">
+                    <img class="img-responsive" id="logo" src="img/CIIEMP-2Invertido.png" alt="">
                   </div>
                   <div class="collapse navbar-collapse js-navbar-collapse">
                     <ul class="nav navbar-nav">
