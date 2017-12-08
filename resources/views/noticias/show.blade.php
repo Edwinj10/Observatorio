6e16d86b7bee96b04460ff88f45b5ad813e6bf59
Edwinj10 <edwinjosealtamirano@gmail.com>
2017-12-07 21:49:53 -0600

    Agregando Sociales

--- resources/views/noticias/show.blade.php
+++ resources/views/noticias/show.blade.php
@@ -3,17 +3,14 @@
 @section('content')
 {!!Html::style('/css/comentario.css')!!}
 <div id="fb-root"></div>
-<meta property="og:img" content="http://www.gabydiaz.com/wp-content/uploads/facebook_debug-1024x840.jpg">
-<div id="fb-root"></div>
+<meta property="og:img" content="http://observatorio.ciiemp.com/imagenes/noticias/{{$noticia->foto}}">
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.11';
+  js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10&appId=1515275818535109";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-
-
 
 <div class="row"> 
   <div class="col-md-12"> 
@@ -38,9 +35,7 @@
     <p>
       {!!$noticia->descripcion!!}
     </p>
-    <div class="fb-share-button"  data-layout="button" data-size="small" data-mobile-iframe="true">
-      <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Compartir</a>
-    </div>
+    <div class="fb-share-button"  data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fobservatorio-edwinjaltamirano.c9users.io%2F&amp;src=sdkpreparse">Compartir</a></div>
   </div>
   <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">           
     <div class="widget-sidebar">
